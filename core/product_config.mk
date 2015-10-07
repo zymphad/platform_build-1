e4497aa35adcb11cb5d2de0d41db610aec5b17be
mike <mikew8978@gmail.com>
2015-10-07 10:35:57 -0400

    Add brunch and a few clean up items

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
 ifneq ($(CUSTOM_BUILD),)
-    all_product_configs := $(shell ls vendor/solo/products/nexus_$(CUSTOM_BUILD).mk)
+    all_product_configs := $(shell ls vendor/solo/products/solo_$(CUSTOM_BUILD).mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
