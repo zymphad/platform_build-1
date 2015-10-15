be356d51eb3e4829ec6e7a29201050cd68cddd5e
beanstown106 <nbenis106@gmail.com>
2015-10-15 08:27:19 -0400

    clean up rom flash & add purenexus flare *drop the target:build fingerprint text *drop the patching system image unconditionally text *drop the verifying system image text *changed the verified system image text *add flashing system text *add some purenexus flare

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -604,8 +604,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
   # Dump fingerprints
-  script.Print("Target: %s" % CalculateFingerprint(
-      oem_props, oem_dict, OPTIONS.info_dict))
+  #script.Print("Target: %s" % CalculateFingerprint(
+  #    oem_props, oem_dict, OPTIONS.info_dict))
+  script.Print("********************")
+  script.Print("**   Pure NeXus   **")
+  script.Print("********************")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
